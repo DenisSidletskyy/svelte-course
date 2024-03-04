7d82c8b16ce6a4ed2d422c039b481fcfb73bfd2d
Denis Sidletskyy <dsidletskyy@gmail.com>
2024-03-04 14:35:06 +0200

    custom usernames

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
-  import AuthCheck from "$lib/components/AuthCheck.svelte";
 </script>
 
 <nav class="flex justify-center my-6">
@@ -31,7 +30,3 @@
     </div>
   </main>
 </AnimatedRoute>
-
-<AuthCheck>
-  Signed in user content only!
-</AuthCheck>
