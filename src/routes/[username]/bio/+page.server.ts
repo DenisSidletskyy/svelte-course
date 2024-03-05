07dc9122823533a71e74699b9b61643743cd8c7e
Denis Sidletskyy <dsidletskyy@gmail.com>
2024-03-05 16:20:58 +0200

    sveltekit actions

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -1,6 +1,6 @@
 import type { PageServerLoad } from "./$types";
 import { adminAuth, adminDB } from "$lib/server/admin";
-import { error, redirect } from "@sveltejs/kit";
+import { error, redirect, type Actions, fail } from "@sveltejs/kit";
 
 export const load = (async ({ locals, params }) => {
 
@@ -22,3 +22,27 @@ export const load = (async ({ locals, params }) => {
       bio,
     };
 }) satisfies PageServerLoad;
+
+export const actions = {
+  default: async ({ request, params, locals }) => {
+    const uid = locals.userID
+
+    const data = await request.formData()
+    const bio = data.get('bio')
+
+    const userRef = adminDB.collection("users").doc(uid!)
+    const {username} = (await userRef.get()).data()! 
+
+    if (params.username !== username) {
+      throw error(401, "That username does not belong to you!")
+    }
+
+    if ((bio as string).length > 260) {
+      return fail(400, {problem: "Bio mus be less than 260 characters"})
+    }
+
+    await userRef.update({
+      bio
+    })
+  }
+} satisfies Actions
